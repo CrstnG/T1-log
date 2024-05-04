bed04664a04ef8872c3cebe6c33b602b71070a6b
Iván Salas Aedo <ivanaureliosalasaedo@gmail.com>
2024-05-04 17:59:45 -0400

    def método ss 04.05

---
+++ Metodo_SS/ss.cpp
@@ -0,0 +1,23 @@
+#include "node.h"
+#include <tuple>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <array>
+#include <random>
+#include <map>
+#include <cmath>
+using namespace std;
+
+/**
+ * Función del método de construcción SS, que permite a partir de un conjunto de puntos, construir el árbol M.
+ * @param puntos Conjunto de puntos a partir de los cuales se construirá el árbol M.
+ * @return Un objeto de tipo Node.
+ */
+Node ss(vector <puntosbd> puntos){
+    // implementación del método de construcción SS
+    int B = 4096/(sizeof(Entry)+sizeof(bool)+sizeof(int));
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# T1-log
+# Tarea 1 - CC4102 Diseño y anáñisis de algoritmos
+
+Repositorio del código de la tarea 1 del ramo CC4102 Diseño y análisis de algoritmos, otoño 2024. 
+
+La tarea consiste en la implementación de un M-Tree, que es un método de acceso dínamico, se incluyen dos métodos de construcción:
+1. Método Ciaccia-Patella (CP): Se encuentra en el archivo `cp.cpp`
+2. Método Sexton-Swinback (SS): Se encuentra en el archivo `ss.cpp`
+
+### Integrantes del grupo:
+- Cristián ...
+- Cristián
+- Iván Salas
